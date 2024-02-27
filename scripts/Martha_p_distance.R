8fe3dfae209cfa8da7efff9bb537c3bbfd806295
marthaluka <mawia.martha@gmail.com>
2024-02-27 18:12:22 +0000

    update base R function

--- scripts/Martha_p_distance.R
+++ scripts/Martha_p_distance.R
@@ -1,15 +1,54 @@
-#install.packages("devtools")
-devtools::install_github("gtonkinhill/pairsnp-r")
 
 
-library(pairsnp)
-fasta.file.name <- system.file("extdata", "seqs.fa", package = "pairsnp")
-sparse.data <- import_fasta_sparse(fasta.file.name)
-d <- snp_dist(sparse.data)
-s <- snp_similarity(sparse.data)
 
 
-# start
+
+# Base R function
+
+
+read_fasta_sequence <- function(fasta_file) {
+  lines <- readLines(fasta_file)
+  # Assuming the first line is the header and subsequent lines are the sequence
+  sequence <- paste(lines[-1], collapse = "")
+  return(sequence)
+}
+
+
+
+
+calc_p_distance <- function(path_to_ref, path_to_query) {
+  # Read fasta file
+  ref <- read_fasta_sequence(path_to_ref)
+  query <- read_fasta_sequence(path_to_query)
+  
+  # Convert the sequences to character vectors
+  ref_chars <- strsplit(ref, split = "")[[1]]
+  query_chars <- strsplit(query, split = "")[[1]]
+  
+  # Count the differences (SNPs)
+  snps <- sum(seq1_chars != seq2_chars)
+  
+  # Calculate p-distance
+  p_dist <- snps/length(ref_chars)
+  
+  # output
+  return(p_dist)
+}
+
+# Example usage
+
+calc_p_distance(path_to_ref = "./data/tmp_ref_b99.fasta", path_to_query = "./data/tmp_query.fasta")
+
+
+
+
+# end 
+
+
+
+
+
+
 
 
 require(pacman)
@@ -22,31 +61,6 @@ query <- readDNAStringSet("./data/tmp_query.fasta")
 ref_header <- names(ref)
 
 query_header <- names(query)
-
-
-read_fasta_sequence <- function(fasta_file) {
-  lines <- readLines(fasta_file)
-  # Assuming the first line is the header and subsequent lines are the sequence
-  sequence <- paste(lines[-1], collapse = "")
-  return(sequence)
-}
-
-# Example usage
-sequence1 <- read_fasta_sequence("./data/tmp_ref_b99.fasta")
-sequence2 <- read_fasta_sequence("./data/tmp_query.fasta")
-
-
-# Convert the sequences to character vectors
-seq1_chars <- strsplit(sequence1, split = "")[[1]]
-seq2_chars <- strsplit(sequence2, split = "")[[1]]
-
-# Count the differences (SNPs)
-snps <- sum(seq1_chars != seq2_chars)
-
-# Print the number of SNPs
-print(snps)
-
-
 #####
 read.fasta <- function(path_to_ref, path_to_seq){
   ref <- readDNAStringSet(path_to_ref)
