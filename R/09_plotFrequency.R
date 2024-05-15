d0b82e64edb092a86996328fd9cf7e999523cceb
Martha Luka <39192976+marthaluka@users.noreply.github.com>
2024-05-15 06:34:34 +0100

    Update 09_plotFrequency.R

--- R/09_plotFrequency.R
+++ R/09_plotFrequency.R
@@ -5,12 +5,12 @@ source("R/02_readFasta.R")
 
 plotFrequency <- function(assignedTypesDF, showLegend=FALSE){
   # Add 'species' column based on the first letter of 'assigned_type'
-  assignedTypesDF$species <- substr(assignedTypesDF$assigned_type, 1, 1)
+  assignedTypesDF$species <- substr(assignedTypesDF$assignedType, 1, 1)
   
   
-  types_counts <- aggregate(query ~ assigned_type, data = assignedTypesDF, FUN = length)
-  types_counts$label <- paste0(types_counts$assigned_type, ", ", types_counts$query)
-  types_counts$species <- substr(types_counts$assigned_type, 1, 1)
+  types_counts <- aggregate(query ~ assignedType, data = assignedTypesDF, FUN = length)
+  types_counts$label <- paste0(types_counts$assignedType, ", ", types_counts$query)
+  types_counts$species <- substr(types_counts$assignedType, 1, 1)
   types_counts <- transform(types_counts,
                             end_y = (query),
                             start_y = c(0, head(cumsum(query), n=-1)))
@@ -23,7 +23,7 @@ plotFrequency <- function(assignedTypesDF, showLegend=FALSE){
   colors <- color_map[types_counts$species]
   
   # Plot the barplot
-  barplot(height = types_counts$query, names.arg = types_counts$assigned_type, col = colors, 
+  barplot(height = types_counts$query, names.arg = types_counts$assignedType, col = colors, 
           main = "Frequency of Types", xlab = "RV Type", ylab = "Count")
   if (showLegend){
     # Add legend
